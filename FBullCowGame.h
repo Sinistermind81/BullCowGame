32d7af359ac94d58abe45f13dfea09d883859cbf
Sinistermind81 <bradkenny81@yahoo.com>
2018-04-05 02:17:42 -0500

    First Commit

---
+++ FBullCowGame.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+
+
+class FBullCowGame
+{
+public:
+	void Reset(); // TODO make a more rich return value.
+	int GetMaxTries();
+	int GetCurrentTry();
+	bool IsGameWone();
+	bool CheckGuessValidity(std::string); // TODO make a more rich return value.
+
+
+// Please try and ignore this and focus on the interface above
+private:
+	int MyCurrentTry = 1;
+	int MyMaxTries = 5;
+};
+
---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include "FBullCowGame.h"
+
+void PrintIntro();
+void PlayGame();
+std::string GetGuess();
+bool AskToPlayAgain();
+
+FBullCowGame BCGame; //instantiate a new game
+
+// entry point of our application
+int main()
+{
+	bool bPlayAgain = false;
+	do
+	{
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	} 
+	while (bPlayAgain);
+
+	return 0; //exit the application
+}
+
+
+// introduce the game
+void PrintIntro() 
+{
+	constexpr int WORLD_LENGTH = 5;
+	std::cout << "Welcome to Bulls and Cows, a fun word game!\n";
+	std::cout << "Can you guess the " << WORLD_LENGTH;
+	std::cout << " letter isogram I'm thinking of?\n";
+	std::cout << std::endl;
+	return;
+}
+
+
+// Plays the game
+void PlayGame()
+{
+	int MaxTries = BCGame.GetMaxTries();
+
+	// loop for the number of turns asking for guesses
+	for (int count = 1; count <= MaxTries; count++)
+	{
+		// print the guess back
+		std::string Guess = GetGuess();
+		std::cout << "Your guess was: " << Guess << std::endl;
+		std::cout << std::endl;
+	}
+}
+
+
+std::string GetGuess() 
+{
+	int CurrentTry = BCGame.GetCurrentTry();
+
+	// get guess from the player
+	std::cout << "Try " << CurrentTry << ". Enter your guess: ";
+	std::string Guess = "";
+	std::getline(std::cin, Guess);
+	return Guess;
+}
+
+
+bool AskToPlayAgain()
+{
+	std::cout << "Do you want to play again? (y/n) ";
+	std::string Response = "";
+	std::getline(std::cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
+}
+
